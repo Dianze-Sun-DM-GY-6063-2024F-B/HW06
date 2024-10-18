3e5067e9f141f8598e0af18dd287e9a28c7c9f42
Denzelllll <denzelsun01@gmail.com>
2024-10-18 13:49:02 -0400

    1

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,21 +1,44 @@
 // TODO: add more circles using lists
 
-let ypos;
+let numCircles;
+let ypos = [];
+let xpos = [];
+let diam = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(30);
   noStroke();
 
-  ypos = -50;
+  numCircles = 10;
+  //numCircles = int(width/25);
+
+  for (let index = 0; index < numCircles; index += 1) {
+    xpos.push(random(width));
+    ypos.push(random(height));
+    diam.push(random(30,100));
+    print(xpos,ypos);
+
+  }
 }
 
 function draw() {
   background(220, 20, 220);
-  ellipse(width / 2, ypos, 50);
-  ypos += 8;
+  //ellipse(width / 2-100, ypos[0], 50);
+  //ellipse(width / 2, ypos[1], 50);
+  //ellipse(width / 2 + 100, ypos[2], 50);
 
-  if (ypos > height) {
-    ypos = -50;
+  for (let index = 0; index < ypos.length; index += 1) {
+    let xpos = map(index, 0, ypos.length-1, 0, width);
+
+    ellipse(xpos + 25, ypos[index], 50);
+    ypos[index] += 8;
+    if (ypos[index] > height) {
+      ypos[index] = random(-60, -30);
+    }
   }
+}
+
+function mousePressed(){
+  ypos.push(random(height));
+  xpos.push(random(height));
 }
--- 1/sketch.js
+++ 1/sketch.js
@@ -1,20 +1,48 @@
-// TODO: add more properties using objects
+// TODO: add more circles using lists
 
-let ypos;
+let ypos0;
+let ypos1;
+let ypos2;
+
+let ypos = [-50,-50,-50];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
 
-  ypos = -50;
+  ypos0 = -50;
+  ypos1 = -50;
+  ypos2 = -50;
 }
 
 function draw() {
-  background(220, 20, 220);
-  ellipse(width / 2, ypos, 50);
-  ypos += 10;
+  background(120, 150, 220);
+  ellipse(width / 2 - 100, ypos0, 50);
+  ellipse(width / 2, ypos1, 50);
+  ellipse(width / 2 + 100, ypos2, 50);
 
-  if (ypos > height) {
-    ypos = -50;
+  ypos0 += 8;
+  ypos1 += 11;
+  ypos2 += 17;
+
+  if (ypos0 > height) {
+    ypos0 = -50;
   }
+
+  if (ypos1 > height) {
+    ypos1 = -50;
+  }
+
+  if (ypos2 > height) {
+    ypos2 = -50;
+  }
+
+
+
+
+
+
+
+
+
 }
